a4db4f353d9c50b587fb9ba8e99de8c8fe4b5ce2
SaulinhoDevs <165098314+saulobmelo@users.noreply.github.com>
2025-07-28 22:23:03 -0300

    Update classe main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,4 +1,18 @@
+import java.net.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        InetSocketAddress p1 = new InetSocketAddress("localhost", 5001);
+        InetSocketAddress p2 = new InetSocketAddress("localhost", 5002);
+        InetSocketAddress p3 = new InetSocketAddress("localhost", 5003);
+
+        Processo proc1 = new Processo("P1", 5001, List.of(p2, p3));
+        Processo proc2 = new Processo("P2", 5002, List.of(p1, p3));
+        Processo proc3 = new Processo("P3", 5003, List.of(p1, p2));
+
+        new Thread(proc1::iniciar).start();
+        new Thread(proc2::iniciar).start();
+        new Thread(proc3::iniciar).start();
     }
 }
